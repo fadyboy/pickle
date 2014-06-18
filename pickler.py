b567ad5e5f6c6b1817c072f6265c1f63c13e6639
anthonyfadero <aofadero@gmail.com>
2014-06-18 23:44:45 +0100

    after first debub

--- pickler.py
+++ pickler.py
@@ -5,18 +5,10 @@ def list_pickler(my_list, my_file):
     '''
         This function takes a list object and pickles it to a file on disk
     '''
-    # assign my_list to a list
-    #my_list = ["one", 33, "two", "jasmine", 45]
+    pickle_file = open(my_file, 'wb')
+    pickle.dump(my_list, pickle_file)
 
-    # create a file object for the pickle in write binary mode
-    my_file = open('pickle_file', 'wb')
-    #
-    # # pickle the list to the file using the pickle dump()
-    pickle.dump(my_list, my_file)
-    #
-    my_file.close()
-    #
-    #return my_pickle
+    pickle_file.close()
 
 
 def unpickler(pickle_file):
@@ -24,13 +16,14 @@ def unpickler(pickle_file):
         This function takes a pickled object and unpickles to a variable
     '''
     # open picked object in read binary mode which is the parameter parsed in
-    pickle_file = open('pickle_file', 'rb')
-    #
+    #pickle_file = open('pickle_file', 'rb')
+    unpickle_file = open(pickle_file, 'rb')
+
     # # unpickle the pickled object by calling the pickle load() and assign to a variable
-    my_unpickle = pickle.load(pickle_file)
+    my_unpickle = pickle.load(unpickle_file)
     #
-    # # close file
-    pickle_file.close()
+    # close file
+    unpickle_file.close()
     # return the my_unpickle object
     return my_unpickle
 
--- pickler_test.py
+++ pickler_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from pickler import list_pickler, unpickler
 # declare a class for my pickle unit tests
 class PickleTests(unittest.TestCase):
@@ -6,18 +7,25 @@ class PickleTests(unittest.TestCase):
     # create a setUp method to always use the following components
     def setUp(self):
         # create a file object for writing to a file in binary
-        self.file_obj = open('testfile', 'wb')
+        self.file_name = 'test_file'
+        #self.file_obj = file_name
 
     # create a teardown method to close the open file object
     def tearDown(self):
-        self.file_obj.close()
+        #self.file_obj.close()
+        pass
 
     # test that an object that is pickled and unpickled have the same data
     def pickle_unpickle_equal_test(self):
 
         my_list = [1, 2, "sherman Wosely"]
-        list_pickler(my_list, self.file_obj)
-        another_list = unpickler(self.file_obj)
+        #list_pickler(my_list, self.file_obj)
+        list_pickler(my_list, self.file_name )
+        another_list = unpickler(self.file_name)
 
         # check that my_list and another list are equal
         self.assertEqual(True, my_list == another_list)
+
+
+
+
